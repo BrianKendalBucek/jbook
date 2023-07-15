0ea41914758c5a3f16750d78f6ef7448310a26eb
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-14 22:36:11 -0700

    got everything working on both localhost 3000 and 4005

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -4,11 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
-var express_1 = __importDefault(require("express"));
-var serve = function (port, filename, dir) {
-    var app = express_1.default();
-    return new Promise(function (resolve, reject) {
-        app.listen(port, resolve).on('error', reject);
+const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
+const serve = (port, filename, dir) => {
+    const app = (0, express_1.default)();
+    app.use((0, http_proxy_middleware_1.createProxyMiddleware)("/", {
+        target: "http://127.0.0.1:3000/",
+        ws: true,
+        changeOrigin: true,
+        logLevel: "silent",
+        headers: {
+            Connection: "keep-alive",
+        },
+    }));
+    return new Promise((resolve, reject) => {
+        app.listen(port, "0.0.0.0", resolve).on("error", reject);
     });
 };
 exports.serve = serve;
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,9 +1,22 @@
-import express from 'express';
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
+  app.use(
+    createProxyMiddleware("/", {
+      target: "http://127.0.0.1:3000/",
+      ws: true,
+      changeOrigin: true,
+      logLevel: "silent",
+      headers: {
+        Connection: "keep-alive",
+      },
+    })
+  );
+
   return new Promise<void>((resolve, reject) => {
-    app.listen(port, resolve).on('error', reject);
+    app.listen(port, "0.0.0.0", resolve).on("error", reject);
   });
 };
