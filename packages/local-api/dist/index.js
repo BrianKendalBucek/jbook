780924cf77aceeca0681c97f6bca106644b74360
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-16 22:18:47 -0700

    set up the packagePath absolute path to the local client build index.html

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,18 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
-const http_proxy_middleware_1 = require("http-proxy-middleware");
+const path_1 = __importDefault(require("path"));
 const serve = (port, filename, dir) => {
     const app = (0, express_1.default)();
-    app.use((0, http_proxy_middleware_1.createProxyMiddleware)("/", {
-        target: "http://127.0.0.1:3000/",
-        ws: true,
-        changeOrigin: true,
-        logLevel: "silent",
-        headers: {
-            Connection: "keep-alive",
-        },
-    }));
+    const packagePath = require.resolve('local-client/build/index.html');
+    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    // app.use(
+    //   createProxyMiddleware("/", {
+    //     target: "http://127.0.0.1:3000/",
+    //     ws: true,
+    //     changeOrigin: true,
+    //     logLevel: "silent",
+    //     headers: {
+    //       Connection: "keep-alive",
+    //     },
+    //   })
+    // );
     return new Promise((resolve, reject) => {
         app.listen(port, "0.0.0.0", resolve).on("error", reject);
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,20 +1,23 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
+import path from 'path';
 
 export const serve = (port: number, filename: string, dir: string) => {
   const app = express();
 
-  app.use(
-    createProxyMiddleware("/", {
-      target: "http://127.0.0.1:3000/",
-      ws: true,
-      changeOrigin: true,
-      logLevel: "silent",
-      headers: {
-        Connection: "keep-alive",
-      },
-    })
-  );
+  const packagePath = require.resolve('local-client/build/index.html');
+  app.use(express.static(path.dirname(packagePath)));
+  // app.use(
+  //   createProxyMiddleware("/", {
+  //     target: "http://127.0.0.1:3000/",
+  //     ws: true,
+  //     changeOrigin: true,
+  //     logLevel: "silent",
+  //     headers: {
+  //       Connection: "keep-alive",
+  //     },
+  //   })
+  // );
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, "0.0.0.0", resolve).on("error", reject);
