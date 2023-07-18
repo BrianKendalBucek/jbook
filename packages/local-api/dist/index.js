b290852662b43b5dd6e56c5e2b556da55d561ece
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-17 21:20:08 -0700

    created useProxy if statement

--- packages/local-api/dist/index.js
+++ packages/local-api/dist/index.js
@@ -5,24 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serve = void 0;
 const express_1 = __importDefault(require("express"));
+const http_proxy_middleware_1 = require("http-proxy-middleware");
 const path_1 = __importDefault(require("path"));
-const serve = (port, filename, dir) => {
+const serve = (port, filename, dir, useProxy) => {
     const app = (0, express_1.default)();
-    // for when the user has installed the cli into their local machine
-    const packagePath = require.resolve('local-client/build/index.html');
-    app.use(express_1.default.static(path_1.default.dirname(packagePath)));
-    // for development mode
-    // app.use(
-    //   createProxyMiddleware("/", {
-    //     target: "http://127.0.0.1:3000/",
-    //     ws: true,
-    //     changeOrigin: true,
-    //     logLevel: "silent",
-    //     headers: {
-    //       Connection: "keep-alive",
-    //     },
-    //   })
-    // );
+    if (useProxy) {
+        app.use((0, http_proxy_middleware_1.createProxyMiddleware)("/", {
+            target: "http://127.0.0.1:3000/",
+            ws: true,
+            changeOrigin: true,
+            logLevel: "silent",
+            headers: {
+                Connection: "keep-alive",
+            },
+        }));
+    }
+    else {
+        const packagePath = require.resolve("local-client/build/index.html");
+        app.use(express_1.default.static(path_1.default.dirname(packagePath)));
+    }
     return new Promise((resolve, reject) => {
         app.listen(port, "0.0.0.0", resolve).on("error", reject);
     });
--- packages/local-api/src/index.ts
+++ packages/local-api/src/index.ts
@@ -1,26 +1,31 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
-import path from 'path';
+import path from "path";
 
-export const serve = (port: number, filename: string, dir: string) => {
+export const serve = (
+  port: number,
+  filename: string,
+  dir: string,
+  useProxy: boolean
+) => {
   const app = express();
 
-  // for when the user has installed the cli into their local machine
-  const packagePath = require.resolve('local-client/build/index.html');
-  app.use(express.static(path.dirname(packagePath)));
-
-  // for development mode
-  // app.use(
-  //   createProxyMiddleware("/", {
-  //     target: "http://127.0.0.1:3000/",
-  //     ws: true,
-  //     changeOrigin: true,
-  //     logLevel: "silent",
-  //     headers: {
-  //       Connection: "keep-alive",
-  //     },
-  //   })
-  // );
+  if (useProxy) {
+    app.use(
+      createProxyMiddleware("/", {
+        target: "http://127.0.0.1:3000/",
+        ws: true,
+        changeOrigin: true,
+        logLevel: "silent",
+        headers: {
+          Connection: "keep-alive",
+        },
+      })
+      );
+    } else {
+    const packagePath = require.resolve("local-client/build/index.html");
+    app.use(express.static(path.dirname(packagePath)));
+  }
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, "0.0.0.0", resolve).on("error", reject);
