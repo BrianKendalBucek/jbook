d4e0fb50da1f93968df2fda133334244883227f2
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-07-19 22:51:20 -0700

    corrected middleware issue

--- packages/local-api/dist/routes/cells.js
+++ packages/local-api/dist/routes/cells.js
@@ -18,38 +18,39 @@ const promises_1 = __importDefault(require("fs/promises"));
 const path_1 = __importDefault(require("path"));
 const createCellsRouter = (filename, dir) => {
     const router = express_1.default.Router();
-    router.use(express_1.default.json());
     const fullPath = path_1.default.join(dir, filename);
     router.get("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         const isLocalApiError = (err) => {
             return typeof err.code === "string";
         };
-        router.get('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-            try {
-                const result = yield promises_1.default.readFile(fullPath, { encoding: 'utf-8' });
-                res.send(JSON.parse(result));
-            }
-            catch (err) {
-                if (isLocalApiError(err)) {
-                    if (err.code === "ENOENT") {
-                        yield promises_1.default.writeFile(fullPath, "[]", "utf-8");
-                        res.send([]);
-                    }
+        try {
+            const result = yield promises_1.default.readFile(fullPath, { encoding: "utf8" });
+            res.send(JSON.parse(result));
+        }
+        catch (err) {
+            if (isLocalApiError(err)) {
+                if (err.code === "ENOENT") {
+                    // Create new file with default cells
+                    yield promises_1.default.writeFile(fullPath, "[]", "utf8");
+                    res.send([]);
                 }
                 else {
                     throw err;
                 }
             }
-        }));
-        router.post('/cells', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-            // Take the list of cells from the request obj
-            // Serialize them
-            const { cells } = req.body;
-            // Write the cells into the file
-            yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
-            res.send({ status: 'ok' });
-        }));
-        return router;
+        }
+        // If reading fails because of missing file
+        // Parse a list of cells from file
+        // Send a list of cells back to the browser
     }));
+    router.post("/cells", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+        // Take the list of cells from the request obj
+        // serialize them
+        const { cells } = req.body;
+        // Write the cells into the file
+        yield promises_1.default.writeFile(fullPath, JSON.stringify(cells), "utf8");
+        res.send({ status: "ok" });
+    }));
+    return router;
 };
 exports.createCellsRouter = createCellsRouter;
--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,56 +1,55 @@
-import express from 'express'
-import fs from 'fs/promises';
-import path from 'path';
-
+import express from "express";
+import fs from "fs/promises";
+import path from "path";
+ 
 interface Cell {
   id: string;
   content: string;
-  type: 'text' | 'code';
+  type: "text" | "code";
 }
-
+ 
 interface LocalApiError {
   code: string;
 }
-
+ 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router();
-  router.use(express.json());
-  
+ 
   const fullPath = path.join(dir, filename);
-
+ 
   router.get("/cells", async (req, res) => {
     const isLocalApiError = (err: any): err is LocalApiError => {
       return typeof err.code === "string";
     };
- 
-  router.get('/cells', async (req, res) => {
     try {
-      const result = await fs.readFile(fullPath, { encoding: 'utf-8'});
-
+      const result = await fs.readFile(fullPath, { encoding: "utf8" });
+ 
       res.send(JSON.parse(result));
     } catch (err) {
       if (isLocalApiError(err)) {
         if (err.code === "ENOENT") {
-          await fs.writeFile(fullPath, "[]", "utf-8");
+          // Create new file with default cells
+          await fs.writeFile(fullPath, "[]", "utf8");
           res.send([]);
+        } else {
+          throw err;
         }
-      } else {
-        throw err;
       }
     }
+    // If reading fails because of missing file
+ 
+    // Parse a list of cells from file
+    // Send a list of cells back to the browser
   });
-    
-
-  router.post('/cells', async (req, res) => {    
+ 
+  router.post("/cells", async (req, res) => {
     // Take the list of cells from the request obj
-    // Serialize them
+    // serialize them
     const { cells }: { cells: Cell[] } = req.body;
-
     // Write the cells into the file
-    await fs.writeFile(fullPath, JSON.stringify(cells), 'utf-8');
-
-    res.send({ status: 'ok' });
+    await fs.writeFile(fullPath, JSON.stringify(cells), "utf8");
+ 
+    res.send({ status: "ok" });
   });
-
   return router;
 };
